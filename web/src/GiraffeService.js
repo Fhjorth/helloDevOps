9ae5e8b530b54979ab5a3d51d20ad27296a37a7d
Frederik hjorth <s175109@student.dtu.dk>
2019-10-05 15:56:43 +0200

    .

--- web/src/GiraffeService.js
+++
@@ -1,9 +0,0 @@
-import {Path} from "@jest/types/build/Config";
-
-@Produces(MediaType.APPLICATION_JSON)
-@Path("giraffes")
-
-public class GiraffeService {
-    List<String> giraffes = Array.asList("Melman", "Elmer");
-    @GET
-}
--- web/src/stores/GiraffeStore.js
+++ web/src/stores/GiraffeStore.js
@@ -4,7 +4,6 @@ import {decorate, observable} from "mobx";
 export default class GiraffeStore {
 
     giraffes = ["Marius","Melman"];
-
 }
 
 decorate(GiraffeStore,{giraffes: observable})
